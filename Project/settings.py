0028a50ab98c56cdf77b717b874955671bc1bdbc
Debendranath Malik <malikdebendra794@gmail.com>
2026-09-17 13:58:54 +0530

    Configure production CORS

--- Project/settings.py
+++ Project/settings.py
@@ -116,7 +116,11 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://dairy-mart-frontend.vercel.app",
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
